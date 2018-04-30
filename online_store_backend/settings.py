a4e642b40152e04e4f14cd256adfc7e9fe228911
kevin-bai <897840029@qq.com>
2018-04-30 12:52:03 +0800

    项目结构配置

--- online_store_backend/settings.py
+++ online_store_backend/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, BASE_DIR)
+sys.path.insert(0, os.path.join(BASE_DIR,'apps'))
+sys.path.insert(0, os.path.join(BASE_DIR,'extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -76,8 +79,14 @@ WSGI_APPLICATION = 'online_store_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'online-store',
+        'USER': 'bjw',
+        'PASSWORD': 'Xiaoshu123.',
+        'HOST': '111.231.94.71',
+        'OPTIONS': {
+            "init_command": "SET default_storage_engine=INNODB",
+        }
     }
 }
 
