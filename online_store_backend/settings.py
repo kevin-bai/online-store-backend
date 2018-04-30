64d31dcddafb8b92566b5366a9d8a39bb161efca
kevin-bai <897840029@qq.com>
2018-04-30 21:36:14 +0800

    time-zone时区设置更改

--- online_store_backend/settings.py
+++ online_store_backend/settings.py
@@ -114,15 +114,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False  # 这里改成false，以后django取时间就不用世界时间，改用本地时间
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
