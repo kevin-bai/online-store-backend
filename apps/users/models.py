c5d28506cbd9ce58b616029c8d5a3973a9f59ee1
kevin-bai <897840029@qq.com>
2018-04-30 15:34:52 +0800

    user model goods model

--- apps/users/models.py
+++ apps/users/models.py
@@ -12,7 +12,7 @@ class UserProfile(AbstractUser):
     mobile = models.CharField(max_length=11, verbose_name='手机')
     gender = models.CharField(max_length=6, choices=(('male', '男'), ('female', '女')), default='male', verbose_name='性别')
     email = models.EmailField(max_length=100, null=True, blank=True, verbose_name='邮箱')
-    image = models.ImageField(upload_to='image/%Y/%m', max_length=150, verbose_name='头像')
+    image = models.ImageField(upload_to='users/', max_length=150, verbose_name='头像')
 
     class Meta:
         verbose_name = '用户'
@@ -26,14 +26,14 @@ class VerifyCode(models.Model):
     """
     短信验证码，正式项目也可以放在redis里，不放数据库
     """
-    code = models.CharField(max_length=10,verbose_name='验证码')
+    code = models.CharField(max_length=10, verbose_name='验证码')
     mobile = models.CharField(max_length=11, verbose_name='手机')
 
     # 一般项目都会有这3个字段，
-    add_time = models.DateField(default=datetime.now,verbose_name='添加时间')
-    update_time = models.DateField(default=datetime.now,verbose_name='更改时间')
+    add_time = models.DateField(default=datetime.now, verbose_name='添加时间')
+    update_time = models.DateField(default=datetime.now, verbose_name='更改时间')
     # 有的数据库做删除，是只做删除标记，没有真的删除
-    is_deleted = models.DateField(default=datetime.now,verbose_name='是否删除')
+    is_deleted = models.DateField(default=datetime.now, verbose_name='是否删除')
 
     class Meta:
         verbose_name = '短信验证码'
--- online_store_backend/settings.py
+++ online_store_backend/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DjangoUeditor',
+    'users',
+    'user_operation',
+    'goods',
+    'trade'
 ]
 
 MIDDLEWARE = [
