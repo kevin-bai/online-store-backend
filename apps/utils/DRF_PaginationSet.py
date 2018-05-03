c264c7af628eb4c14e761e7203c70bb3ab8425e9
kevin-bai <897840029@qq.com>
2018-05-03 15:21:38 +0800

    分页参数名更改

--- apps/utils/DRF_PaginationSet.py
+++ apps/utils/DRF_PaginationSet.py
@@ -15,6 +15,7 @@ class LargeResultsSetPagination(PageNumberPagination):
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
+    page_query_param = 'p'
     max_page_size = 1000
 
 
