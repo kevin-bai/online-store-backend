3c4fad9c44410a6d694ba25c56fcee0d44fb0591
kevin-bai <897840029@qq.com>
2018-05-15 17:40:18 +0800

    购物车

---
+++ apps/trade/serializers.py
@@ -0,0 +1,44 @@
+# _*_ coding:utf-8 _*_
+
+__author__ = 'kevin'
+__date__ = '2018/5/15 16:05'
+
+from rest_framework import serializers
+
+from .models import ShoppingCart
+from goods.models import Goods
+
+
+class ShoppingCartSerializer(serializers.Serializer):
+    """
+    这里不用ModelSerializer,因为当点击加入购物车，此时购物车列表已经存在，这个记录。我们model里面写了unique_together = ("user", "goods")
+    那么会直接抛异常，而我们希望这个记录数量加1
+    所以要自己控制
+    """
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
+    )
+    add_time = serializers.DateTimeField(read_only=True, format="%Y-%m-%d %H:%M")
+    nums = serializers.IntegerField(required=True, min_value=1, error_messages={"required": "请填写数量",
+                                                                                "min_value": "数量至少为1"
+                                                                                })
+    # 这里不是ModelSerializer 在Meta中写好对应的model,需要在参数中指定queryset。
+    goods = serializers.PrimaryKeyRelatedField(queryset=Goods.objects.all(), required=True)
+
+    #
+    def create(self, validated_data):
+        # 在serializer中，requset不直接放在self里，views.py里面 倒是可以直接 self.request.user
+        user = self.context["request"].user
+        nums = validated_data["nums"]
+        goods = validated_data["goods"]
+
+        existed = ShoppingCart.objects.filter(user=user,goods=goods)
+
+        if existed:
+            existed = existed[0]
+            existed.nums += nums
+            existed.save()
+        else:
+            # 这里不用加user？
+            existed = ShoppingCart.objects.create(**validated_data)
+        return existed
--- apps/trade/views.py
+++ apps/trade/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import mixins, viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.authentication import SessionAuthentication
+
+from utils.permissions import IsOwnerOrReadOnly
+from .serializers import ShoppingCartSerializer
+from .models import ShoppingCart
+
+
+class ShoppingCartViewSet(viewsets.ModelViewSet):
+    """
+    购物车功能
+    create:
+        创建购物车记录
+    list:
+        购物车记录列表
+    update:
+        购物车更新
+    delete:
+        删除购物车记录
+    """
+    serializer_class = ShoppingCartSerializer
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
+    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
+
+    def get_queryset(self):
+        return ShoppingCart.objects.filter(user=self.request.user)
